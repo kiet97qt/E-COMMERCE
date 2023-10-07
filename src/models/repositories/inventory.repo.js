c9078d0e6e5587fa36dd8b3aad6183bf5086bd63
kiet-lucents <128343768+kiet-lucents@users.noreply.github.com>
2023-10-07 16:59:20 +0700

    section 19,20

---
+++ src/models/repositories/inventory.repo.js
@@ -0,0 +1,19 @@
+const { inventory } = require("../inventory.model");
+const { Types } = require("mongoose");
+const insertInventory = async ({
+  productId,
+  shopId,
+  stock,
+  location = "unknow",
+}) => {
+  return await inventory.create({
+    inven_productId: productId,
+    inven_stock: stock,
+    inven_location: location,
+    inven_shopId: shopId,
+  });
+};
+
+module.exports = {
+  insertInventory,
+};
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -19,6 +19,7 @@ const {
   updateProductById,
 } = require("../models/repositories/product.repo");
 const { removeUndefinedObject, updateNestedObjectParser } = require("../utils");
+const { insertInventory } = require("../models/repositories/inventory.repo");
 //define Factory class to create product
 
 class ProductFactory {
@@ -118,7 +119,16 @@ class Product {
 
   // create new product
   async createProduct(product_id) {
-    return await product.create({ ...this, _id: product_id });
+    const newProduct = await product.create({ ...this, _id: product_id });
+    if (newProduct) {
+      // add product_stock in inventory collection
+      await insertInventory({
+        productId: newProduct.id,
+        shopId: this.product_shop,
+        stock: this.product_quantity,
+      });
+    }
+    return newProduct;
   }
 
   async updateProduct(product_id, bodyUpdate) {
